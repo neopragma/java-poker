4bd3c7af83d70bfa770021e3a7f5ed05c9ba5ca3
Dave Nicolette <davenicolette@gmail.com>
2018-01-23 16:54:28 -0700

    Modified Group and GroupTest per revised design for scoring hands

--- src/test/java/com/neopragma/poker/GroupTest.java
+++ src/test/java/com/neopragma/poker/GroupTest.java
@@ -7,7 +7,22 @@ public class GroupTest {
 
     @Test(expected=PreconditionNotMetException.class)
     public void it_throws_when_constructor_is_called_with_null_HandValue() {
-        new Group(null, new NullCardSet(), new NullCardSet(), new PlayingCard(Suit.DIAMONDS, Rank.KING));
+        new Group(null, new NullCardSet(), new NullCardSet(), new NullCard());
+    }
+
+    @Test(expected=PreconditionNotMetException.class)
+    public void it_throws_when_constructor_is_called_with_null_high_CardSet() {
+        new Group(HandValue.UNSCORED, null, new NullCardSet(), new NullCard());
+    }
+
+    @Test(expected=PreconditionNotMetException.class)
+    public void it_throws_when_constructor_is_called_with_null_low_CardSet() {
+        new Group(HandValue.UNSCORED, new NullCardSet(), null, new NullCard());
+    }
+
+    @Test(expected=PreconditionNotMetException.class)
+    public void it_throws_when_constructor_is_called_with_null_high_junk_card() {
+        new Group(HandValue.UNSCORED, new NullCardSet(), new NullCardSet(), null);
     }
 
 }
